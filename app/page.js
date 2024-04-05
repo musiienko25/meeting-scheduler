017caa947db1ec157f410a773f0115c6db581b84
musiienko25 <devmusiienko@gmail.com>
2024-04-05 19:51:16 +0300

    main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <main className="">
+      <Header />
       <Button variant="destructive">Subscribe</Button>
     </main>
   );
